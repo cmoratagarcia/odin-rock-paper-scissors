e18912b0f711c608922361b4925dc1d30e6ef8e3
Carmen Morata <cmoratagarcia@gmail.com>
2023-09-12 10:11:44 -0700

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // Begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+let options = ["Rock", "Paper", "Scissors"];
+let randomIndex = Math.floor(Math.random() * 3);
+function getComputerChoice() {
+  return options[randomIndex];
+}
+console.log(getComputerChoice());
 
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
