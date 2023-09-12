d89ce6856a65add0da445dd88df628cae9d2e65a
Carmen Morata <cmoratagarcia@gmail.com>
2023-09-12 11:20:48 -0700

    Move options inside getComputer
    I was leaving them to use as a comparison array, but I will be attempting that separately

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-let options = ["rock", "paper", "scissors"];
-let randomIndex = Math.floor(Math.random() * 3);
-function getComputerChoice() {
-  return options[randomIndex];
-}
-let computerSelection = getComputerChoice();
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-// let playerSelection = userInput.toLowerCase();
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 let playerScore = 0;
 let computerScore = 0;
+
+function getComputerChoice() {
+  let options = ["rock", "paper", "scissors"];
+  let randomIndex = Math.floor(Math.random() * 3);
+  return options[randomIndex];
+}
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a tie!";
@@ -43,6 +42,13 @@ function playRound(playerSelection, computerSelection) {
 // Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 function game() {
   for (let i = 0; i < 5; i++) {
-    playRound();
+    let computerMove = getComputerChoice();
+    // Use prompt() to get input from the user. Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+    let playerMove = prompt("Enter rock, paper or scissors").toLowerCase();
+
+    playRound(playerMove, computerMove);
+    return `You:${playerScore} - Computer:${computerScore}`;
   }
 }
+
+game();
