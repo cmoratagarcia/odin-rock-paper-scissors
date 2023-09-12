78107e6abbe4f5e1157b5d0a88390b76b67262a5
Carmen Morata <cmoratagarcia@gmail.com>
2023-09-12 09:59:43 -0700

    Add project requirements as comments

---
+++ script.js
@@ -0,0 +1,7 @@
+// Begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+
+// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
